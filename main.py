c31ed6eeb74deeeca8ba0fce430acf53628bc085
judemont <96385330+judemont@users.noreply.github.com>
2023-03-01 15:30:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from tkinter import *
 from PIL import Image
 from array import *
+import sys
+
+from tqdm import tqdm
+
+def reduce_resolution(image_path, new_height):
+    with Image.open(image_path) as img:
+    
+        width, height = img.size
+        
+        reduction_factor = new_height / height
+
+        new_width = int(width * reduction_factor)
+
+        img = img.resize((new_width, new_height), resample=Image.BOX)
+       
+        return img
+    
+
+def print_help():
+    print("Usage: python3 main.py [image_path] [height] \n --help or -h for help")
+    exit()
 
 def convert2D(liste):
 
@@ -27,40 +48,47 @@ def convert2D(liste):
 
 
     
-        
+if len(sys.argv) >= 2:
+    if(sys.argv[1] == "--help" or sys.argv[1] == "-h"):
+        print_help()
+    else:
+        if len(sys.argv) == 3:
+            img_filename = sys.argv[1]
+            img_height = int(sys.argv[2])
+
+        else:
+            print_help()
+else:
+    print_help()
 
 
-        
+#try:
 
-
-
-
-img_filename = input("image path : ")
-im = Image.open(img_filename).convert("1")
-
+im = reduce_resolution(img_filename, img_height).convert("1")
+"""
+except:
+    print("Image not found")
+    exit()
+"""
 
 
 pixelles = convert2D(list(im.getdata()))
-print(len(pixelles))
-
-while len(pixelles) >= 10000:
-    print(len(pixelles))
-   
 
 
 
-
-
-print(len(pixelles))
-print(len(pixelles[0]))
 
 result=""
 
 test =0
 
-for i in range(0, len(pixelles)):
+
+
+for i in tqdm( range(0, len(pixelles)), desc="Loading..."):
+   
+
     result += "\n"
     test=0
+
     
     for ii in range(0, len(pixelles[i])):
         
@@ -70,6 +98,7 @@ for i in range(0, len(pixelles)):
             
         else:
             result += "▄▄"
+      
         test+=1
        
     
